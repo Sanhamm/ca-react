cd6111891d1d20054b7fa9531ad2e85a8c525cf2
Sanhamm <sanhamm097@gmail.com>
2023-03-23 14:33:13 +0100

    finnish layout, cart toggle, and burger menu.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout";
+import CartPage from "./pages/CartPage";
+import ContactPage from "./pages/ContactPage";
+import Homepage from "./pages/Homepage";
+import ProductsPage from "./pages/ProductsPage";
+import "./styles/style.css";
+
 function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/Contact' element={<ContactPage />} />
+        <Route path='/Product/:id' element={<ProductsPage />} />
+        <Route path='/Cart' element={<CartPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+import { ConfigProvider } from "antd";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { cart } from "./components/cart";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={cart}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "black",
+        },
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
